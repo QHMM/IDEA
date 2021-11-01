ade79d83f6b5efc306564e128fdfd1e6892ed962
清欢 <wujijun98@gmail.com>
2021-11-01 18:29:32 +0800

    第二次修改

--- github777/src/main/java/com/wjj/test/TestGit.java
+++ github777/src/main/java/com/wjj/test/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("新建git项目！");
         System.out.println("第一次修改！");
+        System.out.println("第二次修改！");
     }
 }
